d3e80604e7c4ea29cec38e6e8f031e2b4a87beef
Icaro Joel <icaromoura.programador@gmail.com>
2022-08-19 18:31:28 -0300

    req 11 complete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function SearchBar() {
+  const [textSearch, setTextSearch] = useState('');
+  const [searchItem, setSearchItem] = useState('');
+  const [filterType, setFilterType] = useState('');
+
+  useEffect(() => {
+    const fetchItem = async (url) => {
+      const data = await fetch(url).then((res) => res.json());
+      return data;
+    };
+
+    if (filterType === 'ingredient') {
+      const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchItem}`;
+      fetchItem(url);
+    }
+    if (filterType === 'name') {
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchItem}`;
+      fetchItem(url);
+    }
+    if (filterType === 'letter') {
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchItem}`;
+      fetchItem(url);
+      if (filterType.length > 1) {
+        global.alert('Your search must have only 1 (one) character');
+      }
+    }
+  }, [filterType, searchItem]);
+
   return (
     <div className="search">
-      <input type="text" data-testid="search-input" />
+      <input
+        type="text"
+        value={ textSearch }
+        onChange={ ({ target }) => setTextSearch(target.value) }
+        data-testid="search-input"
+      />
       <div className="radio-item">
         <label htmlFor="ingredient">
           <input
@@ -11,6 +43,7 @@ function SearchBar() {
             type="radio"
             name="search"
             data-testid="ingredient-search-radio"
+            onClick={ () => setFilterType('ingredient') }
           />
           Ingredient
         </label>
@@ -20,6 +53,7 @@ function SearchBar() {
             type="radio"
             name="search"
             data-testid="name-search-radio"
+            onClick={ () => setFilterType('name') }
           />
           Name
         </label>
@@ -29,11 +63,16 @@ function SearchBar() {
             type="radio"
             name="search"
             data-testid="first-letter-search-radio"
+            onClick={ () => setFilterType('letter') }
           />
           Letter
         </label>
       </div>
-      <button type="button" data-testid="exec-search-btn">
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ () => setSearchItem(textSearch) }
+      >
         Search
       </button>
     </div>
