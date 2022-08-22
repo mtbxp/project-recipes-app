0ce0245fd582e4ec227b876b4cb7440f7eb12229
mtbxp <marcellotb2@gmail.com>
2022-08-22 18:55:37 -0300

    Reqs 57, 58, 59, 60, 61 e 62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile(props) {
-  console.log('fui renderizado');
+  const userLocalStorage = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+  const clearLocalStorage = () => {
+    localStorage.clear();
+  };
+
   return (
     <div>
       <Header { ...props } />
-      Profile
+      { userLocalStorage ? (
+        <h2 data-testid="profile-email">{ userLocalStorage.email }</h2>
+      ) : null }
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        className="profile-buttons"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        className="profile-buttons"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      {' '}
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        className="profile-buttons"
+        onClick={ () => { clearLocalStorage(); history.push('/'); } }
+      >
+        Logout
+      </button>
       <Footer />
     </div>);
 }
