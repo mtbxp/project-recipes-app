436e30b7f86aeac3179649adba3d40b7807ce9d3
Icaro Joel <icaromoura.programador@gmail.com>
2022-08-22 13:09:04 -0300

    terminando o refactor

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,8 @@
-import PropTypes from 'prop-types';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import cardContext from '../context/cardContext';
 
-function SearchBar({ history }) {
-  const { type, setTypeFilter, setSearchItem, recipe } = useContext(cardContext);
+function SearchBar() {
+  const { setTypeFilter, setSearchItem } = useContext(cardContext);
   const [textSearch, setTextSearch] = useState('');
   const [filterType, setFilterType] = useState('');
 
@@ -12,20 +11,6 @@ function SearchBar({ history }) {
     setTypeFilter(filterType);
     setTextSearch('');
   };
-
-  useEffect(() => {
-    if (recipe === null) {
-      global.alert('Sorry, we haven\'t found any recipes for these filters.');
-    }
-    if (recipe !== null && type === 'foods' && recipe.length === 1) {
-      const item = recipe[0].idMeal;
-      history.push(`/${type}/${item}`);
-    }
-    if (recipe !== null && type === 'drinks' && recipe.length === 1) {
-      const item = recipe[0].idDrink;
-      history.push(`/${type}/${item}`);
-    }
-  }, [recipe, type, history]);
 
   return (
     <div className="search">
@@ -76,51 +61,8 @@ function SearchBar({ history }) {
           Search
         </button>
       </header>
-      <section>
-        {
-          recipe !== null && type === 'drinks' && recipe.length > 1
-          && recipe.map((rec, index) => (
-            <div
-              data-testid={ `${index}-recipe-card` }
-              key={ `${index}-recipe-card` }
-            >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ rec.strDrinkThumb }
-                alt={ rec.strDrink }
-              />
-              <h3 data-testid={ `${index}-card-name` }>
-                {rec.strDrink}
-              </h3>
-            </div>
-          ))
-        }
-        {
-          recipe !== null && type === 'foods' && recipe.length > 1
-          && recipe.map((rec, index) => (
-            <div
-              data-testid={ `${index}-recipe-card` }
-              key={ `${index}-recipe-card` }
-            >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ rec.strMealThumb }
-                alt={ rec.strMeal }
-              />
-              <h3 data-testid={ `${index}-card-name` }>
-                {rec.strMeal}
-              </h3>
-            </div>
-          ))
-        }
-      </section>
     </div>
   );
 }
-
-SearchBar.propTypes = {
-  history: PropTypes.shape,
-  location: PropTypes.shape,
-}.isRequired;
 
 export default SearchBar;
