0e708e208fa8801e75b6ae230557e3adfc7f5b0a
Icaro Joel <icaromoura.programador@gmail.com>
2022-08-25 19:17:32 -0300

    na teoria foi

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -5,7 +5,13 @@ function DoneRecipes(props) {
   return (
     <div>
       <Header { ...props } />
-      DoneRecipes
+      <section>
+        <div className="btn">
+          <button type="button" data-testid="filter-by-all-btn">All</button>
+          <button type="button" data-testid="filter-by-food-btn">Food</button>
+          <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+        </div>
+      </section>
     </div>);
 }
 
